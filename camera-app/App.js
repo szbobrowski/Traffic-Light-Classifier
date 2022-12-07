c554e667b095f3877da2bb9d4b6c887e2ff7f7d3
Szymon Bobrowski <szymekbobrowski@wp.pl>
2022-12-07 20:47:05 +0100

    Add simple detector model

--- camera-app/App.js
+++ camera-app/App.js
@@ -8,12 +8,18 @@ import * as FileSystem from 'expo-file-system';
 export default function App() {
   const [option, setOption] = useState('menu');
   const [model, setModel] = useState();
+  const [detector, setDetector] = useState();
 
   const loadModel = async () => {
-    const modelJSON = require('./models/model.json')
-    const modelWeights1 = require('./models/group1-shard1of3.bin')
-    const modelWeights2 = require('./models/group1-shard2of3.bin')
-    const modelWeights3 = require('./models/group1-shard3of3.bin')
+    const modelJSON = require('./classifier-models/model.json');
+    const modelWeights1 = require('./classifier-models/group1-shard1of3.bin');
+    const modelWeights2 = require('./classifier-models/group1-shard2of3.bin');
+    const modelWeights3 = require('./classifier-models/group1-shard3of3.bin');
+
+    const detectorJSON = require('./detector-models/model.json')
+    const detectorWeights1 = require('./detector-models/group1-shard1of3.bin');
+    const detectorWeights2 = require('./detector-models/group1-shard2of3.bin');
+    const detectorWeights3 = require('./detector-models/group1-shard3of3.bin'); 
 
     await tf.ready();
 
@@ -24,12 +30,24 @@ export default function App() {
           modelWeights3
         ])
     ).catch((e) => {
-      console.log("[LOADING ERROR] info:", e);
+      console.log("[LOADING MODEL ERROR] info:", e);
+    })
+
+    const detector = await tf.loadGraphModel(
+        bundleResourceIO(detectorJSON, [
+          detectorWeights1,
+          detectorWeights2,
+          detectorWeights3
+        ])
+    ).catch((e) => {
+      console.log("[LOADING DETECTOR ERROR] info:", e);
     })
     
     setModel(model);
-
     console.log('model loaded');
+
+    setDetector(detector);
+    console.log('detector loaded');
   }
 
   const transformImageToTensor = async (uri) => {
@@ -37,20 +55,19 @@ export default function App() {
       const imgBuffer =  tf.util.encodeString(img64, 'base64').buffer;
       const raw = new Uint8Array(imgBuffer);
       let imgTensor = decodeJpeg(raw);
-      // const scalar = tf.scalar(255);
 
-      imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [48, 48])
+      imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [640, 640])
 
-      // const tensorScaled = imgTensor.div(scalar)
-      const img = tf.reshape(imgTensor, [1,48,48,3])
+      const img = tf.reshape(imgTensor, [1,640,640,3])
       console.log('img', img);
 
-      return img.toFloat();
+      // return img.toFloat();
+      return img.toInt();
   }
 
   const makePredictions = async ( batch, model, imagesTensor ) => {
-    const predictionsdata = model.predict(imagesTensor).arraySync();
-    // let pred = predictionsdata.split(batch);
+    // const predictionsdata = model.predict(imagesTensor).arraySync();
+    const predictionsdata = model.executeAsync(imagesTensor);
      
     return predictionsdata;
   }
@@ -59,9 +76,15 @@ export default function App() {
     await tf.ready();
 
     const tensorImage = await transformImageToTensor(image);
-    const predictions = await makePredictions(1, model, tensorImage);
+    // const predictions = await makePredictions(1, model, tensorImage);
+    const detections = await makePredictions(1, detector, tensorImage);
 
-    console.log(predictions); // bg g r y
+    // console.log(predictions); // bg g r y
+    console.log('1', detections[1].dataSync()[0]);
+    // console.log('2', detections[2].dataSync());
+    // console.log('4', detections[4].dataSync());
+    // console.log('6', detections[6].dataSync());
+    // console.log('7', detections[7].dataSync());
   }
 
   useEffect(() => {
